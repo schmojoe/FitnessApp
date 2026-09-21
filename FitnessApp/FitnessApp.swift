78be053f3c311dae5a4c3545bf165cf56dcab407
Joseph Wood <wood.joseph.m@proton.me>
2026-09-21 16:56:57 -0400

    initial commit

---
+++ FitnessApp/FitnessApp.swift
@@ -0,0 +1,32 @@
+//
+//  FitnessApp.swift
+//  FitnessApp
+//
+//  Created by Joe on 9/21/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FitnessApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
